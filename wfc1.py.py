9b95dee1fb1af99d8cc6cd54bcf6a77db851dd9a
42xxx <mkang4@stevens.edu>
2020-10-01 20:03:28 -0400

    Minghao Kang add tangent line for Fangchi Wu, also, add a test block

--- wfc1.py.py
+++ wfc1.py.py
@@ -4,10 +4,15 @@ import matplotlib.pyplot as plt
 x=np.arange(0,2*np.pi,0.01)
 y_sin=np.sin(x)
 y_cos=np.cos(x)
-
+# add tangent line by Minghao Kang
+y_tan = np.tan(x)
 
 plt.style.use('ggplot')
 plt.plot(x,y_sin,color='r',linewidth=1,alpha=0.6,label="sin(x)")
 plt.plot(x,y_cos,color='b',linewidth=1,alpha=0.6,label='cos(x)')
+plt.plot(x,y_tan,color='g',linewidth=1,alpha=0.6,label='tan(x)')
 plt.legend(bbox_to_anchor=(0.2, 1), loc=1, borderaxespad=0.)
-plt.show()
+plt.ylim(-1,1)
+# may be add a test block
+if __name__ == "__main__":
+    plt.show()
